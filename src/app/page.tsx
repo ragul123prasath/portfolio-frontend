6c842697b39e49670348548d16b0090be62bd9cf
ragul prasath <ragulprasath30@gmail.com>
2025-12-06 17:34:56 +0530

    Initial commit - portfolio frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
       <Blogs />
 
       {/* Pass testimonials into the Testimonials component */}
-      <Testimonials testimonials={testimonials || []} />
+      <Testimonials />
 
       <Experience />
       <Contact />
